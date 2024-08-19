fddfcd527ce9469200caf830b428607fb8571aee
Guney Unay <guneymarketing17@hotmail.com>
2024-08-20 01:28:47 +0300

    :lipstick: update home page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
 import dynamic from "next/dynamic";
+import MaxWidthWrapper from "@/components/ui/max-width-wrapper";
+import { unsplash } from "@/lib/unsplash";
 const TopicList = dynamic(() => import("@/components/topics-list"), {
   ssr: false,
 });
-import MaxWidthWrapper from "@/components/ui/max-width-wrapper";
-import { unsplash } from "@/lib/unsplash";
-import ResponsiveImageGrid from "@/components/responsive-image-grid";
+const TopicDescription = dynamic(
+  () => import("@/components/topic-description"),
+  { ssr: false },
+);
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const { topic: param } = searchParams;
+
+  const topic = await unsplash.topics.get({ topicIdOrSlug: param as string });
+
   return (
     <main className="px-4 md:px-8">
       <MaxWidthWrapper className="h-full w-full">
         <TopicList />
-        {/* <ResponsiveImageGrid images={photos.response} /> */}
+        <TopicDescription topic={topic.response} />
       </MaxWidthWrapper>
     </main>
   );
