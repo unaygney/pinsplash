7368ef5935e8e3c4ca30e6211d5a61593446b9d7
Guney Unay <guneymarketing17@hotmail.com>
2024-08-19 20:06:51 +0300

    :wrench: update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,31 @@ export const toBase64 = (str: string) =>
   typeof window === "undefined"
     ? Buffer.from(str).toString("base64")
     : window.btoa(str);
+
+export const determineAspectRatio = (
+  width: number,
+  height: number,
+): keyof typeof aspectRatios => {
+  const ratio = width / height;
+  const tolerance = 0.3;
+  const aspectRatios: Record<string, number> = {
+    "9/16": 9 / 16,
+    "4/3": 4 / 3,
+    "1/1": 1 / 1,
+  };
+
+  if (Math.abs(ratio - aspectRatios["1/1"]) <= tolerance) {
+    return "1/1";
+  }
+
+  const closestAspectRatio = (
+    Object.keys(aspectRatios) as Array<keyof typeof aspectRatios>
+  ).reduce((prev, curr) => {
+    return Math.abs(ratio - aspectRatios[curr]) <
+      Math.abs(ratio - aspectRatios[prev])
+      ? curr
+      : prev;
+  }, "1/1");
+
+  return closestAspectRatio;
+};
