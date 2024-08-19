eaacd9d5efb04f8535473ab3ba66e34527bba433
Guney Unay <guneymarketing17@hotmail.com>
2024-08-20 01:27:36 +0300

    :hammer: update utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -36,3 +36,13 @@ export const determineAspectRatio = (
 
   return closestAspectRatio;
 };
+export function getLuminance(hexColor: string) {
+  const r = parseInt(hexColor.slice(1, 3), 16) / 255;
+  const g = parseInt(hexColor.slice(3, 5), 16) / 255;
+  const b = parseInt(hexColor.slice(5, 7), 16) / 255;
+
+  const a = [r, g, b].map((v) =>
+    v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4),
+  );
+  return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+}
