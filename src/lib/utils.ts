e7648d6747b99c295ceb3eb05ac595764a4b2a03
Guney Unay <guneymarketing17@hotmail.com>
2024-08-26 18:12:09 +0300

    :wrench: update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,9 +12,10 @@ export const toBase64 = (str: string) =>
 export const determineAspectRatio = (
   width: number,
   height: number,
+  toleranValue = 0.3,
 ): keyof typeof aspectRatios => {
   const ratio = width / height;
-  const tolerance = 0.3;
+  const tolerance = toleranValue;
   const aspectRatios: Record<string, number> = {
     "9/16": 9 / 16,
     "4/3": 4 / 3,
@@ -45,4 +46,16 @@ export function getLuminance(hexColor: string) {
     v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4),
   );
   return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+}
+export function getInitials(name?: string) {
+  if (!name) return "UN";
+
+  const nameParts = name.trim().split(" ");
+  if (nameParts.length === 1) {
+    return nameParts[0][0]?.toUpperCase() || "UN";
+  }
+  return (
+    `${nameParts[0][0]?.toUpperCase() || ""}${nameParts[1][0]?.toUpperCase() || ""}` ||
+    "UN"
+  );
 }
